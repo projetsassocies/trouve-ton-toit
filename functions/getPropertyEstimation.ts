4171db7911fd7b2b0d8e8c4c3ae7649639baf775
projetsassocies <projetsassocies@gmail.com>
2026-03-16 18:08:27 +0100

    maj estime

--- functions/getPropertyEstimation.ts
+++ functions/getPropertyEstimation.ts
@@ -70,7 +70,9 @@ function getAmenityCoeff(amenities: string[] | null | undefined): number {
 
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS, GET',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Max-Age': '86400',
 }
 
 function jsonResponse(data: unknown, status = 200) {
